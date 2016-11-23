6c35b87b3a36da5b4af1ec07c53543efe3426d42
sigmotoa <sigmotoa@misena.edu.co>
2016-11-22 19:56:44 -0500

    Constructor clase Jugador, heredando de clase Persona.

--- src/billar205/Jugador.java
+++ src/billar205/Jugador.java
@@ -10,7 +10,17 @@ package billar205;
  * @author SergioIván
  */
 public class Jugador extends Persona 
-{
+{   /**
+    * en caso de true, es un profesional
+    * en caso de false, es amateur
+    */
+   boolean categoria;
+
+    public Jugador(boolean categoria, String nombre, String id) {
+        super(nombre, id);
+        this.categoria = categoria;
+    }
+   
    
     
     
